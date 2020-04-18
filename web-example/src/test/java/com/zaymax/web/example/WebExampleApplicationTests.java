1190702da745fbef756f50fdbf7afe161a468262
Hu <502636321@gmail.com>
2020-04-18 16:17:07 +0800

    first commit

---
+++ web-example/src/test/java/com/zaymax/web/example/WebExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zaymax.web.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

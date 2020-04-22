177d43148403e19ee2f2755fe53937fc302e5529
Hu <502636321@gmail.com>
2020-04-22 17:39:50 +0800

    1.新增freemarker1-example模块

---
+++ freemarker1-example/src/main/java/com/zaymax/freemarker1/example/Freemarker1ExampleApplication.java
@@ -0,0 +1,13 @@
+package com.zaymax.freemarker1.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Freemarker1ExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Freemarker1ExampleApplication.class, args);
+    }
+
+}
---
+++ freemarker1-example/src/test/java/com/zaymax/freemarker1/example/Freemarker1ExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zaymax.freemarker1.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Freemarker1ExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
